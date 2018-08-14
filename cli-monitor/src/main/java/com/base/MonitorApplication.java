32f2fb1683931d0e8b4fb68312e8e6a778e2551d
hongzh <hzh@hongzh.cc>
2018-08-14 12:32:57 +0800

    Spring Admin 2.0.0

---
+++ cli-monitor/src/main/java/com/base/MonitorApplication.java
@@ -0,0 +1,18 @@
+package com.base;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot启动类
+ * @author hzh 2018/8/14 9:48
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
